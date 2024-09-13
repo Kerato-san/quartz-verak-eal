e9ebcfa3cd31cd6e2ca78f00cf4f4b470cefd71e
Kerato-san <110026538+Kerato-san@users.noreply.github.com>
2024-09-13 21:13:18 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,7 +20,7 @@ const config: QuartzConfig = {
     defaultDateType: "created",
     theme: {
       fontOrigin: "googleFonts",
-      cdnCaching: true,
+      cdnCaching: false,
       typography: {
         header: "Alegreya SC",
         body: "Alegreya Sans",
