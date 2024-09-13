c3f3b9223a4fcf8cc07e1e5ea3ef0709313ede51
Kerato-san <kyllian.franken@gmail.com>
2024-09-13 16:23:07 +0200

    first

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "The World of Verak'eal",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
